5d8eb256e493ce5c444a575c613483ceef8879ef
tmdwo0313 <112990185+tmdwo0313@users.noreply.github.com>
2022-10-17 10:40:26 +0900

    Add files via upload

--- chapter05/c_if_pl2_re.c
+++ chapter05/c_if_pl2_re.c
@@ -3,6 +3,7 @@
 // 학점 계산기 - 리펙토링
 
 int main(void) {
+    // 1.사용자로부터 점수 입력
     int score;
     char grade;
 
@@ -10,18 +11,21 @@ int main(void) {
     scanf("%d", &score);
 
     if(score >= 0 && score <= 100) {
-        // 점수: 0~100점
-        if(score <= 100 && score >= 91) {
+        // 2.점수로부터 등급(A, B, C, ..) 판단
+
+
+        if(score >= 91) {           // score: 100점(91~100)
             grade = 'A';
-        } else if(score <= 90 && score >= 81) {
+        } else if(score >= 81) {    // score: 90점(81~90)
             grade = 'B';
-        } else if(score <= 80 && score >= 71) {
+        } else if(score >= 71) {    // score: 80점(71~80)
             grade = 'C';
-        } else if(score <= 70 && score >= 61) {
+        } else if(score >= 61) {    // score: 70점(61~70)
             grade = 'D';
-        } else if(score <= 60 && score >= 0) {
+        } else {                    // score: 60점(0~60)
             grade = 'F';
         }
+        // 3.점수와 등급을 출력
         printf("학생의 점수는 %d점으로 %c학점입니다.", score, grade);
     } else {
         // 점수: 0점 미만 또는 100점 초과
