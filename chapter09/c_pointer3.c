b2ca4f44d1dc2ee43f179b848a66e2722b0fb917
tmdwo0313 <112990185+tmdwo0313@users.noreply.github.com>
2022-11-23 10:21:53 +0900

    Add files via upload

---
+++ chapter09/c_pointer3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * 일반 변수 vs 포인터 변수
+ * 
+ * int a;    # 21번지~24번지
+ * int *pa;
+ * pa = &a;
+ * *pa = 55;
+ * 
+*/
+
+int main(void) {
+    int a=10, b=15, total; // 31, 32, 33번지
+    double avg;            // 34번지
+    int *pa, *pb;
+    int *pt = &total;   // pt(33번지)
+    double *pg = &avg;  // pg(34번지)
+
+    pa = &a; // pa(31번지)
+    pb = &b; // pb(32번지)
+
+    *pt = *pa + *pb;    // total = 10 + 15
+    *pg = *pt / 2.0;    // avg = 25 / 2.0
+
+    printf("두 정수의 값: %d, %d\n", *pa, *pb);
+    printf("두 정수의 합: %d\n", *pt);
+    printf("두 정수의 평균: %.1lf\n", *pg);
+}
